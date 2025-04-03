a602290a205dfdc22a526d80651f11465cab67f3
michaelgameel5 <michaelgameel15@gmail.com>
2025-04-03 21:47:14 +0200

    First Real Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, SonarQube and GitHub Actions!");
+        System.out.println("First commit to SonarQube and GitHub Actions!");
     }
 }
