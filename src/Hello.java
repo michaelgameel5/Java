b7b91774a6d6489fc974c60f94222a451c4e430a
michaelgameel5 <michaelgameel15@gmail.com>
2025-04-03 21:48:14 +0200

    Second

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, SonarQube and GitHub Actions!");
         System.out.println("First commit to SonarQube and GitHub Actions!");
+        System.out.println("Second commit to SonarQube and GitHub Actions!");
+
+        
     }
 }
