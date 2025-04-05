f37b7f6d9c1c1a666b3c757bae8af65e0a12e986
michaelgameel5 <michaelgameel15@gmail.com>
2025-04-05 17:04:01 +0200

    New Updata

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,13 +9,3 @@ public class Hello {
         
     }
 }
-
-public class DatabaseConnection {
-
-    private static final String PASSWORD = "admin123";  // Vulnerability: S2068
-    
-    public Connection connect() {
-        String url = "jdbc:mysql://localhost:3306/mydb?user=root&password=" + PASSWORD;
-        
-    }
-}
